2e098d3d02d4087996902f39c2d9bc846ad5ba32
filipeas <felipealvessampaio@hotmail.com>
2022-04-07 12:04:00 -0300

    implementacao e ajuste do sistema de comando para init #2

--- bin/index.js
+++ bin/index.js
@@ -3,24 +3,26 @@
 const yargs = require("yargs");
 const create = require("./create.js");
 
-console.log("Bem-vindo ao artisan!");
+console.log("Bem-vindo ao artisan! \nUtilize o comando npx artisan --help para mais informações");
 
-const usage = "\Como usar: artisan <entity> para criar a estrutura no projeto";
+const usage = "\Como usar: $0 <command> [options]";
 
 const options = yargs
     .usage(usage)
-    .option("l", {
-        alias: "create",
-        describe: "Cria estrutura do projeto. \t [create --entity <entity_name>]",
-        // type: "boolean",
-        demandOption: false
-    })
+    .command("--create entity <entity>", "Cria entidade no diretório src/domain/.")
+    .example("npx artisan --create entity User")
+    .array(["create", "init"])
+    .command("--init", "Inicializa projeto com a estrutura padrão do artisan.")
+    .example("npx artisan --init")
     .help(true)
     .argv;
 
-if (yargs.argv._[0] == 'create' && yargs.argv.entity && typeof yargs.argv.entity === 'string') {
-    const entity = yargs.argv.entity[0].toUpperCase() + yargs.argv.entity.substr(1);
+if (yargs.argv.init) {
+    console.log("eontrou do init");
+}
+
+if (yargs.argv.create && yargs.argv.create[0] === "entity") {
+    let entity = yargs.argv.create[1].toLowerCase();
+    entity = entity[0].toUpperCase() + entity.substr(1);
     create.createStructure(entity);
-} else {
-    console.log("O comando para criar uma estrutura é: [artisan create --entity <entity_name>]")
 }
