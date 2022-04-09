54df3fa2fee95f38a86571d84cb87dcbf3a2862d
filipeas <felipealvessampaio@hotmail.com>
2022-04-09 17:44:49 -0300

    implementacao da atividade #10

--- bin/init.js
+++ bin/init.js
@@ -12,17 +12,43 @@ function initStructure() {
 
     // caminhos
     const destinationRaiz = path.join(__dirname, "..", "..", "..", "..");
+    const dirDomain = path.join(__dirname, "..", "..", "..", "..", "src", "domain");
     const destinationInfraSrc = path.join(__dirname, "..", "..", "..", "..", "src");
+    const destinationInfra = path.join(__dirname, "..", "..", "..", "..", "src", "infra");
     const destinationTypes = path.join(__dirname, "..", "..", "..", "..", "src", "@types");
     const destinationTypesExpress = path.join(__dirname, "..", "..", "..", "..", "src", "@types", "express");
-    const destinationInfraHttp = path.join(__dirname, "..", "..", "..", "..", "src", "http");
-    const destinationInfraHttpContainer = path.join(__dirname, "..", "..", "..", "..", "src", "http", "container");
-    const destinationInfraHttpErrors = path.join(__dirname, "..", "..", "..", "..", "src", "http", "errors");
-    const destinationInfraHttpMiddlewares = path.join(__dirname, "..", "..", "..", "..", "src", "http", "middlewares");
-    const destinationInfraHttpRoutes = path.join(__dirname, "..", "..", "..", "..", "src", "http", "routes");
-    const destinationInfraHttpValidation = path.join(__dirname, "..", "..", "..", "..", "src", "http", "validations");
-    const destinationInfraTypeOrm = path.join(__dirname, "..", "..", "..", "..", "src", "typeorm");
-    const destinationInfraTypeOrmMigrations = path.join(__dirname, "..", "..", "..", "..", "src", "typeorm", "migrations");
+    const destinationInfraHttp = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http");
+    const destinationInfraHttpContainer = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http", "container");
+    const destinationInfraHttpErrors = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http", "errors");
+    const destinationInfraHttpMiddlewares = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http", "middlewares");
+    const destinationInfraHttpRoutes = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http", "routes");
+    const destinationInfraHttpValidation = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "http", "validations");
+    const destinationInfraTypeOrm = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "typeorm");
+    const destinationInfraTypeOrmMigrations = path.join(__dirname, "..", "..", "..", "..", "src", "infra", "typeorm", "migrations");
+
+    // verifica se diretorio src/ existe
+    if (fs.existsSync(destinationInfraSrc)) {
+        console.log("src/ já criada.");
+        return;
+    }
+
+    // verifica se diretorio @types/ existe
+    if (fs.existsSync(destinationTypes)) {
+        console.log("@types/ já criada.");
+        return;
+    }
+
+    // verifica se diretorio domain/ ja foi criada
+    if (fs.existsSync(dirDomain)) {
+        console.log("domain/ já criada.");
+        return;
+    }
+
+    // verifica se diretorio infra/ ja foi criada
+    if (fs.existsSync(destinationInfra)) {
+        console.log("infra/ já criada.");
+        return;
+    }
 
     // copiar dependencias no package.json
     createDependencies.copy(destinationRaiz);
